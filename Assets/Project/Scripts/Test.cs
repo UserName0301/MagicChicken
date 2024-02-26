7b2c83a7e0998f7ae44b5cd846ecbe84c7a6dc5d
Hieu <excellentac2k1@gmail.com>
2024-02-26 08:23:05 +0700

    config

--- Assets/Project/Scripts/Test.cs
+++ Assets/Project/Scripts/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // hihi
+        // hi
     }
 
     // Update is called once per frame
